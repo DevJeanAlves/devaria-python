87b232f948914ffcd282d73c7558b523fd809842
JeanAlves2021 <jean.pdolsky2@gmail.com>
2021-04-29 20:41:42 -0300

    aula python-part2

---
+++ main.py
@@ -0,0 +1,30 @@
+if __name__ == '__main__':
+    numero = input("Digite um numero")
+    print(f'O numero digitado foi {numero}')
+
+
+    temperatura = 20
+    print(type(temperatura))
+    temperatura = "Jean"
+    print(temperatura)
+
+    listaNomes = ['Jean', 'Jade', 'Bellinha']
+    print(listaNomes)
+
+    idade = 27
+
+    dicionarioPessoa = {
+        'nome': 'Jean',
+        'idade': idade
+    }
+
+    print(dicionarioPessoa)
+# Comentário
+    numeroComplexo = 5 + 5j
+    print(numeroComplexo)
+
+    verificacao = False
+    print(type(verificacao))
+
+    varNulla = None
+    print(type(varNulla))
